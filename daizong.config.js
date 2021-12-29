e93fb36d0a01c9b7a7d8c22565383b055c057dc2
Mgen <m@mgenware.com>
2021-12-29 15:05:43 +0800

    Move to @web/test-runner-playwright

--- daizong.config.js
+++ daizong.config.js
@@ -1,3 +1,5 @@
+const runTestCmd = 'wtr dist_tests/**/*.test.js --node-resolve --playwright --browsers webkit';
+
 export default {
   // Starts development mode: watches and compiles all source files including tests.
   dev: {
@@ -6,31 +8,25 @@ export default {
   },
 
   // Runs tests (you need to build the project first).
-  t: {
-    run: 'web-test-runner dist_tests/**/*.test.js --node-resolve',
-  },
+  t: runTestCmd,
   // Runs tests in watch mode (you need to build the project first).
-  tw: {
-    run: 'web-test-runner dist_tests/**/*.test.js --node-resolve --watch',
-  },
+  tw: `${runTestCmd} --watch`,
 
-  // Cleans, lints, compiles sources and runs tests.
+  // Cleans, lints, compiles and runs tests.
   build: {
     run: ['#clean', 'tsc -b tests', '#lint', '#t'],
     envGroups: ['production'],
   },
 
-  // Deletes compiled files, auto triggered by `yarn r dev` or `yarn r build`.
+  // Deletes compiled files (auto triggered by `npm run r dev` or `npm run r build`).
   clean: {
     run: {
       del: ['dist', 'dist_tests'],
     },
   },
 
-  // Lints the project using ESLint, auto triggered by `yarn r build`.
-  lint: {
-    run: 'eslint --max-warnings 0 --ext .ts src/ tests/',
-  },
+  // Lints the project using ESLint (auto triggered by `npm run r build`).
+  lint: 'eslint --max-warnings 0 --ext .ts src/ tests/',
 
   _: {
     envGroups: {
--- tests/assert-dom.test.ts
+++ tests/assert-dom.test.ts
@@ -1,21 +1,21 @@
 import { fixture, html, tDOM } from '../dist/main.js';
 
 it('isInlineElement', async () => {
-  const el = await fixture<HTMLElement>(html` <my-element></my-element> `);
+  const el = await fixture<HTMLElement>(html`<my-element></my-element>`);
   tDOM.isInlineElement(el);
 });
 
 it('isBlockElement', async () => {
-  const el = await fixture<HTMLElement>(html` <div></div> `);
+  const el = await fixture<HTMLElement>(html`<div></div>`);
   tDOM.isBlockElement(el);
 });
 
 it('isInlineBlockElement', async () => {
-  const el = await fixture<HTMLElement>(html` <div style="display:inline-block"></div> `);
+  const el = await fixture<HTMLElement>(html`<div style="display:inline-block"></div>`);
   tDOM.isInlineBlockElement(el);
 });
 
 it('isFlexElement', async () => {
-  const el = await fixture<HTMLElement>(html` <div style="display:flex"></div> `);
+  const el = await fixture<HTMLElement>(html`<div style="display:flex"></div>`);
   tDOM.isFlexElement(el);
 });
